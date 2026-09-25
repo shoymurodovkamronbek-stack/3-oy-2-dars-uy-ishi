8dd107c757aea5a36ddd548bc111d7fdc266bb13
shoymurodovkamronbek <shoymurodovkamronbek@gmail.com>
2026-09-25 14:04:40 +0500

    uy ishi

--- uy ishi/func5.js
+++ uy ishi/func5.js
@@ -1,9 +1,15 @@
 const fs = require("fs");
-
 const n = parseInt(fs.readFileSync(0, "utf8").trim());
 
-function sum(n) {
+function summ(n) {
     return Math.floor(n / 100) + Math.floor(n / 10) % 10 + n % 10;
 }
+function jt(n) {
+    if (n % 2 === 0) {
+        console.log("juft");
+    } else {
+        console.log("toq");
+    }
+}
 
-console.log(sum(n));
+jt(summ(n));
